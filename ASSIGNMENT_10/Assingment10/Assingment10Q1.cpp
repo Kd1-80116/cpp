52d1ccd077cdbdfa668ffebcb688c9b25bbef982
Anil71111997 <jataleanil2026@gmail.com>
2023-10-02 23:13:39 +0530

    assignment10_uploded

---
+++ ASSIGNMENT_10/Assingment10/Assingment10Q1.cpp
@@ -0,0 +1,13 @@
+/*
+Q1. Implement a Stack class using C++ templates. Test stack operations on Stack<int>,
+Stack<double> and Stack<Box>.*/
+
+#include<iostream>
+using namespace std;
+
+class Stack
+{
+    int size;
+    int top;
+    
+}
---
+++ ASSIGNMENT_10/Assingment10/Assingment10Q2.cpp
@@ -0,0 +1,2 @@
+/*Q3. Modify assignment 9 – Q2 to save all accounts data into STL list. Provide additional facility to
+display all accounts in forward and reverse order using iterators.*/
